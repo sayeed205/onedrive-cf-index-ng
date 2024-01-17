91280bc008119a43b8b680dbce9c40e362039522
Hitarashi <sayeed205@gmail.com>
2024-01-17 19:17:49 +0530

    Test some changes for cf

--- src/pages/api/auth.ts
+++ src/pages/api/auth.ts
@@ -2,8 +2,14 @@ import type { NextApiRequest, NextApiResponse } from 'next'
 
 export const runtime = 'edge'
 
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+}
+
 export default function handler(_: NextApiRequest, res: NextApiResponse) {
-  res.setHeader('WWW-authenticate', 'Basic realm="Secure Area"')
+  res.setHeader('WWW-authenticate', 'Basic realm="protected"')
   res.statusCode = 401
   res.end(`Auth Required.`)
 }
